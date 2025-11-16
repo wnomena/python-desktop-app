003dc8ff1a10dfa514c5de4244f433376db448d5
Nomena <jsdev.web2@gmail.com>
2025-11-16 09:56:55 +0300

    ajout de quelques fichiers au préalables pour certaines taches

--- Models/DB/pool_creation.py
+++ Models/DB/pool_creation.py
@@ -1,4 +1,8 @@
 from sqlalchemy import create_engine
-class Engine:
+class Mysql_Engine:
     def __init__(self):
-        self._engine = create_engine(f"mysql+pymysql://root:root@localhost:3306/local_caponmada")
+        self._engine = create_engine("mysql+pymysql://root:root@localhost:3306/local_caponmada")
+
+class Sqlite_Engine:
+    def __init__(self):
+        self.engine = create_engine("sqlite3://")
---
+++ index.py
@@ -0,0 +1,7 @@
+
+from customtkinter import CTk
+from sqlalchemy import create_engine
+app = CTk()
+app.minsize(height=500,width=800)
+app.title("Madagascar tours")
+app.mainloop()
